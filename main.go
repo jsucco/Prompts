45ad680e99209361ae77a0679a01424091d306ed
John Joseph Succo <John.Succo@gmail.com>
2017-04-29 13:36:15 -0700

    moved main.go to dir head

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 	"os"
 	"text/template"
-	"bufio"
-	"strings"
 	"controllers"
 	"google.golang.org/appengine"
 )
@@ -18,30 +16,6 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 
 	appengine.Main()
-}
-
-func serveResource(w http.ResponseWriter, req *http.Request) {
-	path := "public" + req.URL.Path
-	var contentType string
-	if strings.HasSuffix(path, ".css") {
-		contentType = "text/css"
-	} else if strings.HasSuffix(path, ".png") {
-		contentType = "image/png"
-	} else {
-		contentType = "text/plain"
-	}
-
-	f, err := os.Open(path)
-
-	if err == nil {
-		defer f.Close()
-		w.Header().Add("Content Type", contentType)
-
-		br := bufio.NewReader(f)
-		br.WriteTo(w)
-	} else {
-		w.WriteHeader(404)
-	}
 }
 
 func populateTemplates() *template.Template {
