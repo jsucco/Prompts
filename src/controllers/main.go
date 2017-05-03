1e1d22ee196ade77cb7e33ca01205e30f1acc53a
John Joseph Succo <John.Succo@gmail.com>
2017-05-03 16:04:52 -0700

    pagable prompts added

--- src/controllers/main.go
+++ src/controllers/main.go
@@ -6,21 +6,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Register(templates *template.Template)  {
+var tpl *template.Template
 
+func Register(templates *template.Template)  {
+	tpl = templates
 	router := mux.NewRouter()
 
 	hc := new(homeController)
 	hc.template = templates.Lookup("home.gohtml")
 	router.HandleFunc("/home", hc.get)
 
-
 	gm := new(gmController)
 	gm.template = templates.Lookup("gm.gohtml")
-	router.HandleFunc("/gm", gm.get)
+	router.HandleFunc("/gm", gm.handle)
+
+	router.HandleFunc("/", idx)
 
 	http.Handle("/", router)
 
 	http.Handle("/public/", http.FileServer(http.Dir(".")))
+
+}
+
+func idx(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w,"home.gohtml", nil)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 
 }
--- src/viewmodels/gm.go
+++ src/viewmodels/gm.go
@@ -1,32 +1,138 @@
 package viewmodels
 
-type gm struct {
+type Gm struct {
 	Title string
-	Prompts []prompt
+	Prompts []Control
+	SelectedPrompt int
+	LastPrompt int
 }
 
-type prompt struct {
-	label string
-	placeholder string
+type Control struct {
+	Label string
+	Value string
+	DataId string
+	Type string
+	IsSelect bool
+	IsInput bool
+	ParentPrompt int
+	Options []option
 }
 
-func GetGM() gm {
+type option struct {
+	Value string
+	Text string
+	Selected bool
+}
 
-	firstPrompt := prompt{
-		label: "Street Address",
-		placeholder: "What is your Street Address?",
-	}
+func GetGM() Gm {
 
-	secondPrompt := prompt{
-		label: "ZIP",
-		placeholder: "What is your ZIP code?",
-	}
-
-	result := gm{
+	result := Gm{
 		Title: "General Questionaire",
-		Prompts: []prompt{
-			firstPrompt,
-			secondPrompt,
+		SelectedPrompt: 1,
+		LastPrompt: 4,
+		Prompts: []Control{
+			Control{
+				DataId: "StreetAddress",
+				Label: "What is your Street Address?",
+				Value: "",
+				IsInput: true,
+				IsSelect: false,
+				Type: "text",
+				ParentPrompt: 1,
+			},
+			Control{
+				DataId: "ZIP",
+				Label: "What is your ZIP code?",
+				Value: "",
+				IsInput: true,
+				IsSelect: false,
+				Type: "number",
+				ParentPrompt: 1,
+			},
+			Control{
+				DataId: "City",
+				Label: "City",
+				Value: "",
+				IsInput: true,
+				IsSelect: false,
+				Type: "text",
+				ParentPrompt: 1,
+			},
+			Control{
+				DataId: "State",
+				Label: "State",
+				Value: "",
+				IsInput: true,
+				IsSelect: false,
+				Type: "text",
+				ParentPrompt: 1,
+
+			},
+			Control{
+				DataId: "InstallDate",
+				Label: "Asset Install Date",
+				Value: "",
+				IsInput: true,
+				IsSelect: false,
+				Type: "date",
+				ParentPrompt: 2,
+			},
+			Control{
+				DataId: "AssetSize",
+				Label: "Asset Size",
+				Value: "Large",
+				IsInput: false,
+				IsSelect: true,
+				Type: "select",
+				ParentPrompt: 3,
+				Options: []option{
+					option{
+						Value: "Large",
+						Text: "Large",
+						Selected: true,
+					},
+					option{
+						Value: "Extra Large",
+						Text: "Extra Large",
+					},
+					option{
+						Value: "Small",
+						Text: "Small",
+					},
+					option{
+						Value: "Medium",
+						Text: "Medium",
+					},
+				},
+			},
+			Control{
+				DataId: "MaintFreq",
+				Label: "Maintenance Frequency",
+				Value: "2 To 3 Times a Year",
+				IsInput: false,
+				IsSelect: true,
+				Type: "select",
+				ParentPrompt: 4,
+				Options: []option{
+					option{
+						Value: "Emergency Repair Only",
+						Text: "Emergency Repair Only",
+					},
+					option{
+						Value: "Once a Year",
+						Text: "Once a Year",
+					},
+					option{
+						Value: "2 to 3 Times a Year",
+						Text: "2 to 3 Times a Year",
+						Selected: true,
+					},
+					option{
+						Value: "4 or More Times a Year",
+						Text: "4 or More Times a Year",
+					},
+				},
+			},
 		},
 	}
 
