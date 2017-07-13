117e9f380e0f2a84ce5c74aadc9f3c473a602753
John Joseph Succo <John.Succo@gmail.com>
2017-07-13 12:02:47 -0700

    login test 2

--- src/controllers/home.go
+++ src/controllers/home.go
@@ -30,29 +30,29 @@ func (this *homeController) login(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content Type", "text/html")
 	vm := viewmodels.GetLogin()
 	if req.Method == "POST" {
-		email := req.FormValue("UserName")
-		password := req.FormValue("PassWord")
+		http.Redirect(w, req, "/survey", 302)
+		//email := req.FormValue("UserName")
+		//password := req.FormValue("PassWord")
 
-		_, err := models.GetMember(email, password)
+		//_, err := models.GetMember(email, password)
 
-		if err == nil {
-			http.Redirect(w, req, "/survey", 302)
-			//session, err_s := models.CreateSession(member)
-			//if err_s == nil {
-			//
-			//	models.SetSessionCookie(w, session.SessionId())
-			//
-			//	http.Redirect(w, req, "/survey", 302)
-			//	return
-			//
-			//} else {
-			//	vm.HasError = true;
-			//	vm.ErrorMsg = "get session - " + err_s.Error();
-			//}
-		} else {
-			vm.HasError = true;
-			vm.ErrorMsg = "get member - " + err.Error();
-		}
+		//if err == nil {
+		//	//session, err_s := models.CreateSession(member)
+		//	//if err_s == nil {
+		//	//
+		//	//	models.SetSessionCookie(w, session.SessionId())
+		//	//
+		//	//	http.Redirect(w, req, "/survey", 302)
+		//	//	return
+		//	//
+		//	//} else {
+		//	//	vm.HasError = true;
+		//	//	vm.ErrorMsg = "get session - " + err_s.Error();
+		//	//}
+		//} else {
+		//	vm.HasError = true;
+		//	vm.ErrorMsg = "get member - " + err.Error();
+		//}
 	}
 
 	this.loginTemplate.Execute(w, vm)
