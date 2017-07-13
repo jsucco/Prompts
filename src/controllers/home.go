2d22f7ee2dadf68f00658c2c59e1c302a9e714d2
John Joseph Succo <John.Succo@gmail.com>
2017-07-13 12:00:42 -0700

    login test 1

--- src/controllers/home.go
+++ src/controllers/home.go
@@ -33,21 +33,22 @@ func (this *homeController) login(w http.ResponseWriter, req *http.Request) {
 		email := req.FormValue("UserName")
 		password := req.FormValue("PassWord")
 
-		member, err := models.GetMember(email, password)
+		_, err := models.GetMember(email, password)
 
 		if err == nil {
-			session, err_s := models.CreateSession(member)
-			if err_s == nil {
-
-				models.SetSessionCookie(w, session.SessionId())
-
-				http.Redirect(w, req, "/survey", 302)
-				return
-
-			} else {
-				vm.HasError = true;
-				vm.ErrorMsg = "get session - " + err_s.Error();
-			}
+			http.Redirect(w, req, "/survey", 302)
+			//session, err_s := models.CreateSession(member)
+			//if err_s == nil {
+			//
+			//	models.SetSessionCookie(w, session.SessionId())
+			//
+			//	http.Redirect(w, req, "/survey", 302)
+			//	return
+			//
+			//} else {
+			//	vm.HasError = true;
+			//	vm.ErrorMsg = "get session - " + err_s.Error();
+			//}
 		} else {
 			vm.HasError = true;
 			vm.ErrorMsg = "get member - " + err.Error();
